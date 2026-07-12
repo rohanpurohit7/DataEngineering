f7c7376ba8c767a134236a7cec6c3b33fc474e9b
Rohan Purohit <rpurohit01@gmail.com>
2026-07-12 16:14:11 -0400

    Delete data-engineering-techniques directory

--- data-engineering-techniques/jobs/hadoop/mapper.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import csv, sys
-reader=csv.DictReader(sys.stdin)
-for row in reader:
-    print(f"{row['event_type']}\t1")
--- data-engineering-techniques/scripts/bootstrap/install_python_dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install boto3 pandas numpy scikit-learn matplotlib pyarrow
